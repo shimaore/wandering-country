9b47905a64556c9785f659338aed1a3453930d57
Stephane Alnet <stephane@shimaore.net>
2018-01-24 22:09:51 +0100

    as-built

--- design.js
+++ design.js
@@ -4,7 +4,7 @@
 
   app = 'wandering-country';
 
-  app_version = '1.0';
+  app_version = '1.2';
 
   p_fun = require('coffeescript-helpers').p_fun;
 
@@ -44,16 +44,29 @@
         },
         devices: {
           map: p_fun(extra, function(doc) {
-            var account;
+            var account, ref;
             if (!((doc.type != null) && doc.type === 'device')) {
               return;
             }
             if (doc.disabled) {
               return;
             }
-            if (doc.account != null) {
-              account = normalize_account(doc.account);
-              emit(['account', account], doc.device);
+            account = doc.account != null ? normalize_account(doc.account) : null;
+            emit(['account', account], doc.device);
+            if ((ref = doc.lines) != null) {
+              ref.forEach(function(arg1) {
+                var domain, e, endpoint, name, ref1;
+                endpoint = arg1.endpoint;
+                if (endpoint == null) {
+                  return;
+                }
+                emit(['endpoint', endpoint], doc.device);
+                e = endpoint.match(/^([^@]+)@(.+)$/);
+                if (e != null) {
+                  ref1 = [e[1], e[2]], name = ref1[0], domain = ref1[1];
+                  return emit(['domain', domain, name], doc.device);
+                }
+              });
             }
           })
         },
@@ -66,76 +79,106 @@
             if (doc.disabled) {
               return;
             }
-            if (doc.account != null) {
-              account = normalize_account(doc.account);
-              emit(['account', account], doc.number_domain);
-            }
+            account = doc.account != null ? normalize_account(doc.account) : null;
+            emit(['account', account], doc.number_domain);
           })
         },
         endpoints: {
           map: p_fun(extra, function(doc) {
-            var account, m;
+            var account, domain, e, name, ref;
             if (!((doc.type != null) && doc.type === 'endpoint')) {
               return;
             }
             if (doc.disabled) {
               return;
             }
-            if (doc.account != null) {
-              account = normalize_account(doc.account);
-              emit(['account', account], doc.endpoint);
+            account = doc.account != null ? normalize_account(doc.account) : null;
+            emit(['account', account], doc.endpoint);
+            if (doc.sub_account != null) {
+              emit(['sub_account', account, doc.sub_account], doc.endpoint);
             }
-            m = doc._id.match(/^endpoint:[^@]+@(.+)$/);
-            if ((m != null ? m[1] : void 0) != null) {
-              emit(['domain', m[1]], doc.endpoint);
+            e = doc._id.match(/^endpoint:([^@]+)@(.+)$/);
+            if (e != null) {
+              ref = [e[1], e[2]], name = ref[0], domain = ref[1];
+              emit(['domain', domain, name], doc.endpoint);
             }
             if (doc.number_domain != null) {
               emit(['number_domain', doc.number_domain], doc.endpoint);
+            }
+            if (doc.location != null) {
+              emit(['location', doc.location], doc.endpoint);
             }
           })
         },
         local_numbers: {
           map: p_fun(extra, function(doc) {
-            var account, m;
+            var account, domain, e, m, name, ref, ref1, ref2, ref3, ref4, ref5;
             if (!((doc.type != null) && doc.type === 'number')) {
               return;
             }
-            if (!(m = doc._id.match(/^number:[^@]+@(.+)$/))) {
+            if (!(m = doc._id.match(/^number:([^@]+)@(.+)$/))) {
               return;
             }
             if (doc.disabled) {
               return;
             }
-            if (doc.account != null) {
-              account = normalize_account(doc.account);
-              emit(['account', account], doc.number);
-            }
-            if (m[1] != null) {
-              emit(['number_domain', m[1]], doc.number);
-            }
+            account = doc.account != null ? normalize_account(doc.account) : null;
+            emit(['account', account], doc.number);
             if (doc.endpoint != null) {
               emit(['endpoint', doc.endpoint], doc.number);
+              e = doc.endpoint.match(/^([^@]+)@(.+)$/);
+              if (e != null) {
+                ref = [e[1], e[2]], name = ref[0], domain = ref[1];
+                emit(['domain', domain, name], doc.number);
+              }
+            }
+            if (doc.user_database != null) {
+              emit(['user_database', doc.user_database], doc.number);
+            }
+            ref1 = [m[1], m[2]], name = ref1[0], domain = ref1[1];
+            emit(['number_domain', domain, name], doc.number);
+            if ((ref2 = doc.groups) != null) {
+              ref2.forEach(function(group) {
+                return emit(['group', domain, group], doc.number);
+              });
+            }
+            if ((ref3 = doc.allowed_groups) != null) {
+              ref3.forEach(function(group) {
+                return emit(['allowed_group', domain, group], doc.number);
+              });
+            }
+            if ((ref4 = doc.queues) != null) {
+              ref4.forEach(function(queue) {
+                return emit(['queue', domain, queue], doc.number);
+              });
+            }
+            if ((ref5 = doc.skills) != null) {
+              ref5.forEach(function(skill) {
+                return emit(['skill', domain, skill], doc.skill);
+              });
             }
           })
         },
         global_numbers: {
           map: p_fun(extra, function(doc) {
-            var account, m;
+            var account, domain, m, n, name, ref;
             if (!((doc.type != null) && doc.type === 'number')) {
               return;
             }
-            if (m = doc._id.match(/^number:[^@]+@(.+)$/)) {
+            if (!(m = doc._id.match(/^number:([^@]+)$/))) {
               return;
             }
             if (doc.disabled) {
               return;
             }
-            if (doc.account != null) {
-              account = normalize_account(doc.account);
-              emit(['account', account]);
-            }
+            account = doc.account != null ? normalize_account(doc.account) : null;
+            emit(['account', account], doc.number);
             if (doc.local_number != null) {
-              emit(['local_number', doc.local_number]);
+              emit(['local_number', doc.local_number], doc.number);
+              if (n = doc.local_number.match(/^([^@]+)@(.+)$/)) {
+                ref = [n[1], n[2]], name = ref[0], domain = ref[1];
+                emit(['number_domain', domain, name], doc.number);
+              }
             }
           })
         }
